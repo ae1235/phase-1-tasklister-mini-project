cef678b3b1126eb596dde1430b633424e3aaa8e3
+ <+>
2022-06-17 16:11:59 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById('create-task-form')
+  form.addEventListener('submit', function(e){
+    e.preventDefault()
+    
+    p = document.createElement('p');
+    p.textContent = `${e.target['new-task-description'].value} `
+    button = document.createElement('button')
+    button.textContent = 'X'
+    form.appendChild(p)
+    p.appendChild(button)
+
+    
+    button.addEventListener('click', function(e){
+      e.target.parentNode.remove()
+    })
+   
+  })
+  
+  
+  
+
 });
+
